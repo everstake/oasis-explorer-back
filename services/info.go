edac04676197ab64c484f6d8fd9149cd2f68be3d
weltburger <XDB8FsLQDGDkK7jJkf52>
2023-04-28 22:01:33 +0300

    rollback

--- services/info.go
+++ services/info.go
@@ -4,40 +4,58 @@ import (
 	"go.uber.org/zap"
 	"oasisTracker/common/log"
 	"oasisTracker/smodels"
+	"time"
 )
 
 const usdCurrency = "usd"
+const getInfoEP = "/data/info"
 
 func (s *ServiceFacade) GetInfo() (info smodels.Info, err error) {
 
-	block, err := s.dao.GetLastBlock()
+	raw, ok, err := s.apiCache.Get(getInfoEP)
 	if err != nil {
 		return info, err
 	}
 
-	ratio, isFound := s.cache.Get(topEscrowCacheKey)
-	if !isFound {
-		ratio, err = s.getTopEscrowRatio()
+	if !ok {
+		block, err := s.dao.GetLastBlock()
 		if err != nil {
-			log.Error("failed to get staking ratio: ", zap.Error(err))
+			return info, err
 		}
-		s.cache.Set(topEscrowCacheKey, ratio, cacheTTL)
-	}
 
-	marketInfo, err := s.marketDataProvider.GetOasisMarketData(usdCurrency, s.cfg.CMC.Key)
-	if err != nil {
-		return info, err
-	}
+		ratio, isFound := s.cache.Get(topEscrowCacheKey)
+		if !isFound {
+			ratio, err = s.getTopEscrowRatio()
+			if err != nil {
+				log.Error("failed to get staking ratio: ", zap.Error(err))
+			}
+			s.cache.Set(topEscrowCacheKey, ratio, cacheTTL)
+		}
 
-	return smodels.Info{
-		Height:      block.Height,
-		TopEscrow:   ratio.(float64),
-		Price:       marketInfo.GetPrice(),
-		PriceChange: marketInfo.GetPriceChange(),
-		MarketCap:   marketInfo.GetMarketCap(),
-		Volume:      marketInfo.GetVolume(),
-		Supply:      marketInfo.GetSupply(),
-	}, nil
+		marketInfo, err := s.marketDataProvider.GetOasisMarketData(usdCurrency, s.cfg.CMC.Key)
+		if err != nil {
+			return info, err
+		}
+
+		info = smodels.Info{
+			Height:      block.Height,
+			TopEscrow:   ratio.(float64),
+			Price:       marketInfo.GetPrice(),
+			PriceChange: marketInfo.GetPriceChange(),
+			MarketCap:   marketInfo.GetMarketCap(),
+			Volume:      marketInfo.GetVolume(),
+			Supply:      marketInfo.GetSupply(),
+		}
+
+		err = s.apiCache.Save(getInfoEP, info, time.Second*30)
+		if err != nil {
+			return info, err
+		}
+
+		return info, nil
+	} else {
+		return raw.(smodels.Info), err
+	}
 }
 
 func (s *ServiceFacade) getTopEscrowRatio() (float64, error) {
